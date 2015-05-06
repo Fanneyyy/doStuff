33a63fb2292e1f5eb17f426b02e66cfead3f479e
ironpeak <Hrafnorri1207@gmail.com>
2015-05-06 15:05:50 +0000

    Finished Skeleton

--- doStuff/Databases/DatabaseGroup.cs
+++ doStuff/Databases/DatabaseGroup.cs
@@ -2,11 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using doStuff.POCOs;
 
 namespace doStuff.Databases
 {
     public class DatabaseGroup : DatabaseBase
     {
-        //TODO
+        public List<UserInfo> GetMembers(uint groupId)
+        {
+            return null;
+        }
+
+        public List<EventInfo> GetEvents(uint groupId)
+        {
+            return null;
+        }
+
+        public bool AddMember(uint groupId, uint memberId)
+        {
+            return false;
+        }
+
+        public bool RemoveMember(uint groupId, uint memberId)
+        {
+            return false;
+        }
+
+        public bool ChangeName(uint groupId, string newName)
+        {
+            return false;
+        }
+
+        public bool CreateGroup(GroupInfo group)
+        {
+            return false;
+        }
+
+        public bool RemoveGroup(uint groupId)
+        {
+            return false;
+        }
     }
 }
--- doStuff/Databases/DatabaseUser.cs
+++ doStuff/Databases/DatabaseUser.cs
@@ -2,11 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using doStuff.POCOs;
 
 namespace doStuff.Databases
 {
     public class DatabaseUser : DatabaseGroup
     {
-        //TODO
+        public List<UserInfo> GetFriends(uint userId)
+        {
+            return null;
+        }
+
+        public List<EventInfo> GetEvents(uint userId)
+        {
+            return null;
+        }
+
+        public bool CreateFriendRequest(uint senderId, uint receiverId)
+        {
+            return false;
+        }
+
+        public bool AnswerFriendRequest(uint senderId, uint receiverId, bool answer)
+        {
+            return false;
+        }
+
+        public bool RemoveFriend(uint senderId, uint receiverId)
+        {
+            return false;
+        }
+
+        public bool ChangeName(uint userId, string newName)
+        {
+            return false;
+        }
     }
 }
