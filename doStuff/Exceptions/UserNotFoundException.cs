aef6e14d815bed7b4c5e23e2cfbeba0dd8a08d6a
helgir <helgir87@gmail.com>
2015-05-07 12:20:14 +0000

    Created Exceptions, Group & Event

---
+++ doStuff/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace doStuff.Exceptions
+{
+    public class UserNotFoundException : Exception
+    {
+        public UserNotFoundException()
+            : base()
+        {
+
+        }
+
+        public UserNotFoundException(string message)
+            : base(message)
+        {
+
+        }
+
+        public UserNotFoundException(string message, Exception inner)
+            : base(message, inner)
+        {
+
+        }
+    }
+}
