0bb699360ea716e2dd24f677fa96a797a7b78e63
fanneyyy <fanneyyyy@gmail.com>
2015-05-07 14:33:16 +0000

    Register Look, vantar input fields

--- doStuff/Models/AccountViewModels.cs
+++ doStuff/Models/AccountViewModels.cs
@@ -46,17 +46,17 @@ namespace doStuff.Models
     public class RegisterViewModel
     {
         [Required]
-        [Display(Name = "User name")]
+        [Display(Name = "username")]
         public string UserName { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "password")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
+        [Display(Name = "confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
