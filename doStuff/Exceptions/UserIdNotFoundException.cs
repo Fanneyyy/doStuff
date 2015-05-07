f9b373335abd6a0a9a8b77ff84ae08f89f804e92
helgir <helgir87@gmail.com>
2015-05-07 11:49:29 +0000

    Cleaning service, and creating exception folder

---
+++ doStuff/Exceptions/UserIdNotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace doStuff.Exceptions
+{
+    public class UserIdNotFoundException : Exception
+    {
+
+        public UserIdNotFoundException()
+            : base()
+        {
+
+        }
+    }
+}
