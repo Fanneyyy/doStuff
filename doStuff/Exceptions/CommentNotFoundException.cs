9f018117220516e0ab11f49d699ea3002bb81f0a
helgir <helgir87@gmail.com>
2015-05-07 17:40:07 +0000

    Bjó til tvö önnur Exception, og fór að kasta villum í service

---
+++ doStuff/Exceptions/CommentNotFoundException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace doStuff.Exceptions
+{
+    public class CommentNotFoundException : Exception
+    {
+
+        public CommentNotFoundException()
+            : base()
+        {
+
+        }
+
+        public CommentNotFoundException(string message)
+            : base(message)
+        {
+
+        }
+
+        public CommentNotFoundException(string message, Exception inner)
+            : base(message, inner)
+        {
+
+        }
+    }
+}
---
+++ doStuff/Exceptions/FriendNotFoundException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace doStuff.Exceptions
+{
+    public class FriendNotFoundException : Exception
+    {
+
+        public FriendNotFoundException()
+            : base()
+        {
+
+        }
+
+        public FriendNotFoundException(string message)
+            : base(message)
+        {
+
+        }
+
+        public FriendNotFoundException(string message, Exception inner)
+            : base(message, inner)
+        {
+
+        }
+
+    }
+}
