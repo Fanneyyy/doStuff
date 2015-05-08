c9842aa0dc438bc169711611247806830c2532e1
kthorri <kristinn.thorri@gmail.com>
2015-05-08 18:02:17 +0000

    Sma fixup í UserController

--- doStuff/Controllers/UserController.cs
+++ doStuff/Controllers/UserController.cs
@@ -27,103 +27,17 @@ namespace doStuff.Controllers
             // Returns the feed to the view
 
             return View(feed);
-/*
-            User newUser = new User();
-            newUser.BirthYear = 25;
-            newUser.Active = true;
-            newUser.DisplayName = "Svenni hundur";
-            newUser.Email = "svennihundur@doggie.is";
-            newUser.Gender = Gender.MALE;
-            newUser.UserID = 01;
-            newUser.UserName = "svennidog";
-
-            Group group1 = new Group(), group2 = new Group();
-            group1.Active = true;
-            group1.GroupID = 01;
-            group1.Name = "Bolti";
-            group1.OwnerId = 01;
-
-            group2.Active = true;
-            group2.GroupID = 02;
-            group2.Name = "Karfa";
-            group2.OwnerId = 01;
-
-            List<Group> groups = new List<Group>();
-            groups.Add(group1);
-            groups.Add(group2);
-
-            Event newEvent1 = new Event();
-            Event newEvent2 = new Event();
-            newEvent1.EventID = 01;
-            newEvent1.Name = "Blindafyllerí";
-            newEvent1.Description = "Það er djamm í kvöld og það er djamm á morgun, og ekki á morgun heldur hinn";
-            newEvent1.Active = true;
-            newEvent1.CreationTime = new DateTime(2015, 5, 05);
-            newEvent1.Location = "heima";
-            newEvent2.EventID = 02;
-            newEvent2.Name = "Spólukúr";
-            newEvent2.Description = "Allir að 'kúra' saman heima hjá þér, eru ekki allir til í það?";
-            newEvent2.Active = true;
-            newEvent2.CreationTime = new DateTime(2015, 5, 06);
-            newEvent2.Location = "heima hja ther";
-
-            EventFeedViewModel feed = new EventFeedViewModel();
-
-            List<Event> events = new List<Event>();
-            events.Add(newEvent1);
-            events.Add(newEvent2);
-
-            Comment comment = new Comment();
-            comment.Active = true;
-            comment.CommentID = 01;
-            comment.Content = "YEAHHH!";
-            comment.CreationTime = new DateTime(2015, 5, 06);
-            comment.OwnerId = 01;
-
-            List<Comment> comments = new List<Comment>();
-            comments.Add(comment);
-            comments.Add(comment);
-
-
-            List<EventViewModel> eventViews = new List<EventViewModel>();
-            
-
-            foreach (Event eachEvent in events)
-            {
-                EventViewModel eventView = new EventViewModel();
-                eventView.Owner = "svennidog";
-                eventView.Event = eachEvent;
-                eventView.Comments = comments;
-                eventViews.Add(eventView);
-            }
-
-            List<User> users = new List<User>();
-
-            SideBarViewModel sidebar = new SideBarViewModel();
-            sidebar.User = newUser;
-            users.Add(newUser);
-            users.Add(newUser);
-            sidebar.UserList = users;
-            feed.Events = eventViews;
-            feed.Groups = groups;
-            feed.SideBar = sidebar;
-            return View(feed);
- * */
         }
 
         [HttpGet]
         public ActionResult AddFriend()
         {
-            //TODO
-
             return View();
         }
 
         [HttpPost]
         public ActionResult AddFriend(User newFriend)
         {
-            //TODO
-               
                 try
                 {
                    int friendId = service.GetUserId(newFriend.UserName);
@@ -137,13 +51,11 @@ namespace doStuff.Controllers
                     ModelState.AddModelError("Error", "Username not found");
                     return View();
                 }
-                
         }
 
         [HttpGet]
         public ActionResult ViewFriendRequests()
         {
-            //TODO
             return View();
         }
 
@@ -152,9 +64,8 @@ namespace doStuff.Controllers
         {
             //TODO
             int myId = service.GetUserId(User.Identity.Name);
-            service.AnswerFriendRequest(userId, myId, answer);
-            
-            return View();
+            service.AnswerFriendRequest(userId, myId, answer); 
+            return RedirectToAction("Index");
         }
 
         [HttpPost]
@@ -173,11 +84,10 @@ namespace doStuff.Controllers
         }
 
         [HttpPost]
-        public ActionResult CreateEvent(/*[Bind(Include = "Id,Title,Text,DateCreated,Category")], */Event newEvent)
+        public ActionResult CreateEvent(Event newEvent)
         {
             if (ModelState.IsValid)
             {
-                //EventID, GroupID, OwnerId, Name, Photo, Description, CreationTime, TimeOfEvent, Minutes, Location, Min, Max
                 newEvent.CreationTime = DateTime.Now;
                 newEvent.OwnerId = service.GetUserId(User.Identity.Name);
                 newEvent.Minutes = 23;
@@ -193,44 +103,39 @@ namespace doStuff.Controllers
         public ActionResult RemoveEvent(int eventId)
         {
             //TODO
+            if (service.RemoveEvent(eventId))
+            {
+                return RedirectToAction("Index");
+            }
             return View();
         }
 
         [HttpPost]
         public ActionResult Comment(int eventId, Comment myComment)
         {
-            //TODO
             service.CreateComment(eventId, myComment);
-
             return View();
         }
 
         [HttpGet]
         public ActionResult ChangeUserName()
         {
-            //TODO
-
             return View();
         }
 
         [HttpPost]
         public ActionResult ChangeUserName(User myUser)
         {
-            //TODO
             int myId = service.GetUserId(User.Identity.Name);
             service.ChangeDisplayName(myId, myUser.DisplayName);
-
             return RedirectToAction("Index");
         }
 
         [HttpPost]
         public ActionResult AnswerEvent(int eventId, bool answer)
         {
-            //TODO
-
             int myId = service.GetUserId(User.Identity.Name);
             service.AnswerEvent(myId, eventId, answer);
-            
             return View();
         }
     }
