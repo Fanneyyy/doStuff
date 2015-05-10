dfc5d17e94a09c99fd2c583f7315e654f188e988
kthorri <kristinn.thorri@gmail.com>
2015-05-10 16:16:26 +0000

    Comment Viewmodel class added + commented views

---
+++ doStuff/ViewModels/CommentViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using doStuff.Models.DatabaseModels;
+
+namespace doStuff.ViewModels
+{
+    public class CommentViewModel
+    {
+        public User Owner;
+        public Comment Comment;
+    }
+}
