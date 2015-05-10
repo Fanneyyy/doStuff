d1a2d14c63561de613cb33f1b6e7700a2673e16f
fanneyyy <fanneyyyy@gmail.com>
2015-05-10 21:00:55 +0000

    Countdown, shows minutes left on refresh

--- doStuff/Scripts/scripts.js
+++ doStuff/Scripts/scripts.js
@@ -7,13 +7,16 @@
 
 function countdown() {
     var countdownElements = $(".countdown");
-    var created;
+    var created = "";
     for (var i = 0; i < countdownElements.length; i++) {
         created = $(countdownElements[i]).data("created");
+        if (typeof created === "string") {
+            created = parseInt(created.split(",")[0]);
+        }
         var date = new Date(created);
         var dateNow = new Date();
         var diff = Math.abs(dateNow - date);
         var minutes = Math.floor(diff / 1000 / 60);
-        $(countdownElements[i]).text(minutes);
+        $(countdownElements[i]).text(23 - minutes);
     }
 }
