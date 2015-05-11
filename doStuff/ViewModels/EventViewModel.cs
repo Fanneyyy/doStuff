d5b1d7a4bd58f60af8a317e47ba688225b333829
kthorri <kristinn.thorri@gmail.com>
2015-05-11 12:25:42 +0000

    EventViewModel

--- doStuff/ViewModels/EventViewModel.cs
+++ doStuff/ViewModels/EventViewModel.cs
@@ -6,11 +6,14 @@ using doStuff.Models.DatabaseModels;
 
 namespace doStuff.ViewModels
 {
+    public enum StateOfEvent { CONDITIONSNOTREACHED, CONDITIONSREACHED, EVENTFULL, EVENTON, EVENTOFF };
+
     public class EventViewModel
     {
         public string Owner;
         public Event Event;
         public bool? Attending;
         public List<CommentViewModel> CommentsViewModels;
+        public List<User> Attendees;
     }
 }
