aefd44d894df20566d76ef962d4532c0d2a9d4b5
fanneyyy <fanneyyyy@gmail.com>
2015-05-11 14:16:41 +0000

    Comment-owner overflow

--- doStuff/Scripts/scripts.js
+++ doStuff/Scripts/scripts.js
@@ -1,45 +1,45 @@
 ﻿$(function () {
 
     $("#datepicker").datetimepicker({ dateFormat: 'dd/mm/yy' });
-
-    //countdown();
+    countdown();
 });
 
 function countdown() {
-    function checkTime() {
-        var elements = $(".countdown");
-        for (item in elements) {
-            debugger;
-            created = $(elements[item]).data("created");
-            if (typeof created === "string") {
-                created = parseInt(created.split(",")[0]);
-            }
-            var initialTime = new Date(created);
-            var timeDifference = 1380000 - (Date.now() - initialTime);
-            if (timeDifference < 0) {
-                $(elements[item]).innerHTML = "ITS ON!";
-            } else {
-                var formatted = convertTime(timeDifference);
-                $(elements[item]).innerHTML = '' + formatted;
-            }
-        }
-    }
-
     //function checkTime() {
-    //    var element = document.getElementById('time');
-    //    created = $(element).data("created");
-    //    if (typeof created === "string") {
-    //        created = parseInt(created.split(",")[0]);
-    //    }
-    //    var initialTime = new Date(created);
-    //    var timeDifference = 1380000 - (Date.now() - initialTime);
-    //    if (timeDifference < 0) {
-    //        element.innerHTML = "ITS ON!";
-    //    } else {
-    //        var formatted = convertTime(timeDifference);
-    //        element.innerHTML = '' + formatted;
+    //    debugger;
+    //    var elements = $(".countdown");
+    //    for (var item in elements) {
+    //        debugger;
+    //        created = $(elements[item]).data("created");
+    //        if (typeof created === "string") {
+    //            created = parseInt(created.split(",")[0]);
+    //        }
+    //        var initialTime = new Date(created);
+    //        var timeDifference = 1380000 - (Date.now() - initialTime);
+    //        if (timeDifference < 0) {
+    //            $(elements[item]).innerHTML = "ITS ON!";
+    //        } else {
+    //            var formatted = convertTime(timeDifference);
+    //            $(elements[item]).innerHTML = '' + formatted;
+    //        }
     //    }
     //}
+
+    function checkTime() {
+        var element = document.getElementById('time');
+        created = $(element).data("created");
+        if (typeof created === "string") {
+            created = parseInt(created.split(",")[0]);
+        }
+        var initialTime = new Date(created);
+        var timeDifference = 1380000 - (Date.now() - initialTime);
+        if (timeDifference < 0) {
+            element.innerHTML = "ITS ON!";
+        } else {
+            var formatted = convertTime(timeDifference);
+            element.innerHTML = '' + formatted;
+        }
+    }
 
     function convertTime(miliseconds) {
         var totalSeconds = Math.floor(miliseconds / 1000);
